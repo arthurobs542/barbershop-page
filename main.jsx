9c4aec6222915a1d108934b089982a88c0ec9d71
Arthur Robson <arthurobson.contato@gmail.com>
2025-08-27 01:37:27 -0300

    puplicando

--- main.jsx
+++ main.jsx
@@ -4,7 +4,6 @@ import App from "./src/pages/App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import Servicos from "./src/pages/Servicos.jsx";
-import logo from "/vite.svg";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
